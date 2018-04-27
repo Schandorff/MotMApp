0caade42cc84c5ef365286046fe3dad5b44bc793
Simon Schandorff <Schandorff1@hotmail.com>
2018-04-27 23:00:34 +0200

    fixed headers

--- SingleClub.xaml.cs
+++ SingleClub.xaml.cs
@@ -12,15 +12,10 @@ namespace Manofthematch
     public partial class SingleClub : ContentPage
     {
         readonly Club currentClub;
-<<<<<<< HEAD
         readonly Authorization manager = new Authorization();
         readonly IList<Team> teams = new ObservableCollection<Team>();
         readonly IList<Match> teamMatches = new ObservableCollection<Match>();
-
-=======
         readonly ApiMethods apiMethods = new ApiMethods();
-        public IList<Team> teams = new ObservableCollection<Team>();
->>>>>>> b90ff636aa92857aa77afb90c8d2aa5e20756ecc
 
         Club requestedClub = new Club();
 
@@ -36,39 +31,26 @@ namespace Manofthematch
             base.OnAppearing();
             requestedClub = await apiMethods.GetClub("GetCLub", currentClub.clubId);
             List<Team> ClubTeams = requestedClub.Teams;
-            
+
             foreach (Team team in ClubTeams)
             {
                 if (teams.All(b => b.teamId != team.teamId))
                     teams.Add(team);
-                
-<<<<<<< HEAD
-               
 
-                foreach (Match match in team.teamMatches)
-                {
-                    teamMatches.Add(match);
-
-=======
-                IList<Match> teamMatches = new ObservableCollection<Match>();
-                if (teamMatches.Count != 0) { 
-                    foreach (Match match in team.teamMatches)
+                    if (teamMatches.Count != 0)
                     {
-                        teamMatches.Add(match);
+                        foreach (Match match in team.teamMatches)
+                        {
+                            teamMatches.Add(match);
+                        }
                     }
->>>>>>> b90ff636aa92857aa77afb90c8d2aa5e20756ecc
+
                 }
+                gameList.ItemsSource = teamMatches;
+                BindingContext = teams;
+
+                clubName.Text = currentClub.clubName;
 
             }
-            gameList.ItemsSource = teamMatches;
-            BindingContext = teams;
-<<<<<<< HEAD
-
-            clubName.Text = currentClub.clubName;
-
-=======
-            clubName.Text = currentClub.clubName;            
->>>>>>> b90ff636aa92857aa77afb90c8d2aa5e20756ecc
         }
-    }
 }
