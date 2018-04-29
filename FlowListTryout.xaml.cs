358efd0796ae114d84edfe2efc38f8e1f6dd6b7b
npes <npesdk@gmail.com>
2018-04-29 16:37:37 +0200

    Clubpage sorting done

--- FlowListTryout.xaml.cs
+++ FlowListTryout.xaml.cs
@@ -11,13 +11,15 @@ using Plugin.Connectivity;
 using System.Diagnostics;
 using DLToolkit.Forms.Controls;
 using DLToolkit.Forms;
+using System.Collections.ObjectModel;
 
 namespace Manofthematch
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FlowListTryout : ContentPage
     {
-        public FlowObservableCollection<Club> allTestClubs = new FlowObservableCollection<Club>();
+        public FlowObservableCollection<Club> allClubs = new FlowObservableCollection<Club>();
+        public FlowObservableCollection<Object> clubsSortedBySport = new FlowObservableCollection<Object>();
         public List<Club> clubCollection = new List<Club>();
 
         readonly ApiMethods apiMethods = new ApiMethods();
@@ -26,29 +28,7 @@ namespace Manofthematch
         {
             NavigationPage.SetHasNavigationBar(this, false); //remove default navigation
             InitializeComponent();
-        }
-
-        async void OnClubSelect(object sender, ItemTappedEventArgs e)
-        {
-            if (!CrossConnectivity.Current.IsConnected)
-            {
-                Debug.WriteLine($"No connection");
-            }
-            else
-            { 
-                await Navigation.PushAsync(new SingleClub((Club)e.Item));
-            }
-        }
-
-        //public Command ItemSelectedCommand = new Command<SelectedItemChangedEventArgs>((arg) =>
-        //{
-        //    var item = arg?.SelectedItem as Club;
-        //    if (item != null)
-        //    {
-        //        //SelectedItem = null;
-        //        //item.Command?.Execute(null);
-        //    }
-        //});
+        }        
 
         protected async override void OnAppearing()
         {
@@ -64,7 +44,7 @@ namespace Manofthematch
                 try
                 {
                     clubCollection = await apiMethods.GetAllClubs("GetAllCLubs", 1083);
-                    allTestClubs = new FlowObservableCollection<Club>(clubCollection);
+                    allClubs = new FlowObservableCollection<Club>(clubCollection); //cast List<Club> to FlowObservableCollection
                     
                 }
                 finally
@@ -73,7 +53,63 @@ namespace Manofthematch
                 }
             }
 
-            TestClubXamlList.FlowItemsSource = clubCollection;
+            
+
+            clubsSortedBySport = await SortBySports(allClubs, "Soccer");
+            TestClubXamlList.FlowItemsSource = clubsSortedBySport;
+        }
+
+        async void OnClubSelect(object sender, ItemTappedEventArgs e)
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                Debug.WriteLine($"No connection");
+            }
+            else
+            {
+                await Navigation.PushAsync(new SingleClub((Club)e.Item));
+            }
+        }
+
+        async Task<FlowObservableCollection<Object>> SortBySports (FlowObservableCollection<Club> allClubs, string sportstype)
+        {
+            List<Club> clubs = new List<Club>(allClubs);
+            //IEnumerable<Club> _sortedClubsSport = new List<Club>();
+            FlowObservableCollection<Club> sortedClubsSport = new FlowObservableCollection<Club>();
+            //clubs = allClubs;
+
+            var __sortedClubsSport = clubs.Where(a => a.clubSports.Contains(sportstype))
+                                    .OrderBy(a => a.clubRegion)
+                                    .GroupBy(a => a.clubRegion)                                    
+                                    .Select(itemGroup => new Grouping<string, Club>(itemGroup.Key, itemGroup))
+                                    .ToList(); //  .Where(c => c.clubSports.Contains(sportstype));
+
+            //IOrderedEnumerable<Club> _sortedClubsSport = new ObservableCollection<Club>(__sortedClubsSport);
+            var Items = new FlowObservableCollection<Object>(__sortedClubsSport);
+            return Items;
+        }
+
+        public class Grouping<K, T> : FlowObservableCollection<T>
+        {
+            public K Key { get; private set; }
+            public int ColumnCount { get; private set; }
+
+            public Grouping(K key)
+            {
+                Key = key;
+            }
+
+            public Grouping(K key, IEnumerable<T> items)
+                : this(key)
+            {
+                AddRange(items);
+            }
+
+            public Grouping(K key, IEnumerable<T> items, int columnCount)
+                : this(key, items)
+            {
+                ColumnCount = columnCount;
+            }
         }
     }
 }
